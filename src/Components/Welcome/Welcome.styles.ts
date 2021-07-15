a54257f7d29a081730d04e72ad2c885daaf6c853
PROLOVE <proLove0212@gmail.com>
2021-07-15 17:57:15 +0900

    Welcome page edits

--- src/Components/Welcome/Welcome.styles.ts
+++ src/Components/Welcome/Welcome.styles.ts
@@ -1,12 +1,57 @@
 import styled from 'styled-components'
 
 import welcomeImage from './welcome.jpg'
+import welcomeImageMobile from './welcome_mobile3.jpg'
+
+const BREAKPOINT_WIDTH = "700px"
+const BREAKPOINT_HEIGHT = "700px"
 
 export const WelcomeArea = styled.div`
+    display: flex;
     min-height: 100vh;
     background: url(${welcomeImage}) no-repeat center center; 
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
     background-size: cover;
+
+    @media (max-width: ${BREAKPOINT_WIDTH}) {
+        background: url(${welcomeImageMobile}) no-repeat center center;
+        background-size: 700px;
+        justify-content: center;
+    }
+    @media (max-height: ${BREAKPOINT_HEIGHT}) and (max-width: ${BREAKPOINT_WIDTH}) {
+        background-size: 400px;
+    }
+`
+export const TextBox = styled.div`
+    padding: 8rem 0 0 8rem;
+    @media (max-width: ${BREAKPOINT_WIDTH}) {
+        padding: 10vh 0 0 0;
+    }
+`
+
+export const BigTitle = styled.h1`
+    font-size: 3rem;
+    margin-bottom: 1rem;
+    padding: 0 0 1rem 0.7rem;
+    border-bottom: 1px solid #2F2F2F;
+    display: inline-block;
+    @media (max-width: ${BREAKPOINT_WIDTH}) {
+        padding-bottom: 0;
+    }
+    @media (max-height: ${BREAKPOINT_HEIGHT}) {
+        font-size: 2rem;
+        margin-top: 3rem;
+        margin-bottom: 0;
+    }
+`
+
+export const SmallerTitle = styled.h3`
+    margin: 0;
+    font-weight: lighter;
+    font-size: 1.5rem;
+    @media (max-width: ${BREAKPOINT_WIDTH}) {
+        text-align: center;
+    }
 `
--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 //Styles
 import { WelcomeArea } from './Welcome.styles'
+import { TextBox, BigTitle, SmallerTitle } from './Welcome.styles'
 
 const Welcome = () => {
     return(
         <WelcomeArea>
-            
+            <TextBox>
+                <BigTitle>岡崎さり</BigTitle>
+                <SmallerTitle>三味線・箏</SmallerTitle>
+            </TextBox>
         </WelcomeArea>
     )
 }
