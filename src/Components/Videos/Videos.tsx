0c3ffdc67dc26de11515e4338125dcc7809b9d67
PROLOVE <proLove0212@gmail.com>
2021-08-19 21:15:53 +0900

    lazy load

--- src/Components/Videos/Videos.tsx
+++ src/Components/Videos/Videos.tsx
@@ -40,8 +40,8 @@ const Videos = () => {
                 <SubTitle color="#2F2F2F">Video</SubTitle>
                 <FlexContainer>
                     {/*<Video width="560" height="315" src="https://www.youtube.com/embed/9yszjIoIAyU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></Video>*/}
-                    <Video width="560" height="315" src="https://www.youtube.com/embed/iWhA8UcEQ7Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></Video>
-                    <Video width="560" height="315" src="https://www.youtube.com/embed/OxLiPuR_wDQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></Video>
+                    <Video loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/iWhA8UcEQ7Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></Video>
+                    <Video loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/OxLiPuR_wDQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></Video>
                 </FlexContainer>
             </FadeInSection>
             
